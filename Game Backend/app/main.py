e3745dfa458c8c9f8e55538fabb539f2dd000b66
Sayan Mondal <sayanmondal2098@gmail.com>
2025-02-07 01:09:14 +0530

    add comodity unsuccessful

--- Game Backend/app/main.py
+++ Game Backend/app/main.py
@@ -5,7 +5,7 @@ from app.routes.stock_routes import stock_router
 from app.routes.chatbot_routes import chatbot_router
 from app.routes.crypto_routes import crypto_router
 from app.routes.options_routes import options_router
-
+from app.routes.commodities_routes import commodities_router
 from app.database import engine, Base
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -27,6 +27,7 @@ Base.metadata.create_all(bind=engine)
 app.include_router(user_router, prefix="/users", tags=["Users"])
 app.include_router(stock_router, prefix="/stocks", tags=["Stocks"])
 app.include_router(chatbot_router, prefix="/chatbot", tags=["Chatbot"])
+app.include_router(commodities_router, prefix="/commodities", tags=["Commodities"])
 app.include_router(scenario_router, prefix="/scenarios", tags=["Scenarios"])
 app.include_router(crypto_router, prefix="/crypto", tags=["Crypto"])
 app.include_router(options_router, prefix="/optiontread", tags=["Options"])
